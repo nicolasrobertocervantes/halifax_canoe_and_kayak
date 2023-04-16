3717b3a5f6b46f73cd951cfca15f5112592c6980
nicolasrobertocervantes <nicolasrobertocervantes@gmail.com>
2023-04-16 17:49:50 -0300

    Adventures

--- index.php
+++ index.php
@@ -5,7 +5,36 @@
         <div class="container">
             <h2>Come Experience<br>Canada</h2>
         </div>
-        
+    </section>
+    <section class="adventures">
+        <div class="container">
+            <h3>Upcoming Adventures</h3>
+        </div>
+        <hr class="line">
+        <div class="adventures__content">
+            <div class="container">
+                <ul>
+                    <li>
+                        <a href="#" class="title">Halifax</a>
+                        <div class="date">Date: 2023-04-12</div>
+                        <div class="duration">Duration: 4 days</div>
+                        <h4>Summary</h4>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </li>
+                    <li>
+                        <a href="#" class="title">Sydney</a>
+                        <div class="date">Date: 2023-06-12</div>
+                        <div class="duration">Duration: 2 days</div>
+                        <h4>Summary</h4>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </section>
     
     <?php include 'includes/footer.php';?>
